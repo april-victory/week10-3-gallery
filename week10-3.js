658ef93beb48cce6562d43b66de5d4bb12630f2d
april-victory <apple.vika@yandex.ru>
2023-11-20 17:22:38 +0300

    dogs gallery

---
+++ week10-3.js
@@ -0,0 +1,8 @@
+const img = document.querySelector(".images");
+const next = () => {
+  img.src = "./assets/images/dog2.jpg";
+};
+
+const back = () => {
+  img.src = "./assets/images/dog1.jpg";
+};
